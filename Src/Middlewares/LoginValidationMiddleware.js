72ede965368e90def04ce3aa0e6d32384d04ab4f
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-06-01 11:26:13 +0530

    Update LoginValidationMiddleware.js

--- Src/Middlewares/LoginValidationMiddleware.js
+++ Src/Middlewares/LoginValidationMiddleware.js
@@ -1,23 +1,43 @@
-import {userModel} from "../Models/User/UserCollection.js";
-import {loginModel} from "../Models/Login/LoginCollection.js";
+import { userModel } from "../Models/User/UserCollection.js";
+import { loginModel } from "../Models/Login/LoginCollection.js";
+import { passwordValidation } from "../Services/Password/PasswordServices.js";
 
-const loginValidationMW = async(req, res, next)=>{
+const loginValidationMW = async (req, res, next) => {
 
     try {
-        const { userInput, password} = req.body;
-        if(!userInput || !password) return res.status(400).json({error: "All fields are required !"});
+        const { userInput, password } = req.body;
+        if (!userInput || !password) return res.status(400).json({ error: "All fields are required !" });
 
         const user = await userModel.findOne({
             $or: [
-                {emailAddress: userInput},
-                {username: userInput}
+                { emailAddress: userInput },
+                { username: userInput }
             ]
         });
-        if(!user) return res.status(404)
-        
+        if (!user) return res.status(404).json({ error: "User not found !" });
+
+        const loginData = await loginModel.findOne({ _id: user._id });
+        if (loginData) {
+            if (loginData.userAgent === req.headers["user-agent"]) {
+                const token = await user.generateToken();
+                res.cookie("login", token, {
+                    maxAge: 24 * 60 * 60
+                });
+                res.status(200).redirect("/");
+
+            } else return res.status(409).json({ error: `${user.username} is already login other device` });
+
+        } else {
+            const validation = await passwordValidation(password, user.password);
+            if (!validation) return res.status(401).json({ error: "Wrong Password !" });
+
+            req.user = user;
+            next();
+        }
+
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: error.message});
+        res.status(500).json({ error: error.message });
     }
 }
 
