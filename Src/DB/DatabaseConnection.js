ebdd53a1fc421b3f7fad1f8c1ea0679655d001ce
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-06-01 11:26:42 +0530

    removing extra files

--- Src/DB/DatabaseConnection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-
-const databaseConnection = mongoose.connect(process.env.DATABASE_CONNECTION)
-                                   .then(()=> console.log("Database Is Connected successfully"))
-                                   .catch((err)=> console.log("Error :", err.stack))
--- Src/Services/Token/TokenValidation.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-async function tokenValidation(token) {
-    const validation = jwt.verify(token, process.env.SECRET_KEY);
-    return validation
-}
-
-export { tokenValidation }
