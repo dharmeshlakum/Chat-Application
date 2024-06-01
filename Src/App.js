55b2eb27f4f49ddcc0eff7a24164237b102d8360
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-06-01 17:46:01 +0530

    updating file

---
+++ Src/App.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createServer } from "node:http";
+import { Server  } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const port = process.env.PORT || 6000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const staticFilePath = join(__dirname, "../Public");
+const imageFilePath = join(__dirname,"../Assets");
+const viewFilePath = join(__dirname, "../Views");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "hbs");
+app.set("views", viewFilePath);
+app.use("/Public", express.static(staticFilePath));
+app.use("/Assets", express.static(imageFilePath));
+
+io.on("connection", (socket)=>{
+    console.log("New user is connected with socketID :", socket.id);
+
+});
+
+server.listen(port,()=> console.log(`Server is running at :- http://localhost:${port}`))
